e51a53dc8d1e3f91d5c01b12a778f57c3d692b81
kaitukarikuto <103973951+kaitukarikuto@users.noreply.github.com>
2023-01-15 13:49:51 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
-function imgchange5() {document.getElementById('logo1').src ="./image/josai.png";}
+function imgchange5() {document.getElementById('logo1').src ="./image/josai.jpg";}
 function imgchange6() {document.getElementById('logo1').src ="./image/josai3.png";}
